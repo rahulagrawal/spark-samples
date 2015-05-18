f41892b1addcc6957fa12843983c146a3b5f65a5
Rahul Agrawal <rahulagrawal@gmail.com>
2015-05-18 16:51:35 +0530

    getting twitter auth properties from a properties file

--- src/main/scala/streaming/twitter/Authenticator.scala
+++ src/main/scala/streaming/twitter/Authenticator.scala
@@ -1,21 +1,52 @@
 package streaming.twitter
 
-import scala.collection.mutable
+import org.apache.spark.Logging
+
+import scala.io.Source
 
 /**
  *
  * @author Rahul Agrawal
  *         Date: 5/16/2015
  */
-object Authenticator {
+object Authenticator extends Logging {
+
+  val propertiesFile: String = "/twitter.properties"
+
+
+  def main(args: Array[String]) {
+
+    val properties = getAuthPropertiesFromFile()
+    setAuthProperties(properties)
+
+  }
+
+  private def foo(s: String): (String, String) = {
+    val pair: Array[String] = s.split("=")
+    (pair(0), pair(1))
+  }
+
+  def getAuthPropertiesFromFile(): Map[String, String] = {
+    val lines = Source.fromURL(getClass.getResource(propertiesFile)).getLines()
+    val keyValue = lines.filter(line => line.split("=").size == 2).map(x => foo(x))
+    keyValue.toMap
+  }
+
+  def setAuthProperties(propMap: Map[String, String]) = {
+    propMap.foreach {
+                      case (key, value) =>
+                        logInfo(s"Key=$key : value=$value")
+                        System.setProperty(key, value)
+                    }
+  }
+
 
   /** Configures the Oauth Credentials for accessing Twitter */
-  def configureTwitterCredentials(apiKey: String, apiSecret: String, accessToken: String, accessTokenSecret: String) {
+  /*def configureTwitterCredentials(apiKey: String, apiSecret: String, accessToken: String, accessTokenSecret: String) {
     val configs = new mutable.HashMap[String, String] ++= Seq("apiKey" -> apiKey,
                                                               "apiSecret" -> apiSecret,
                                                               "accessToken" -> accessToken,
                                                               "accessTokenSecret" -> accessTokenSecret)
-    //println(s"Configuring Twitter OAuth for $configs")
     configs.foreach { case (key, value) =>
       if (value.trim.isEmpty) {
         throw new Exception("Error setting authentication - value for " + key + " not set")
@@ -25,6 +56,6 @@ object Authenticator {
       println("\tSystem Property " + fullKey + " set as [" + value.trim + "]")
                     }
     println()
-  }
+  }*/
 
 }
